ea61ef05f580a6db25ea0ddc52e738e1f6205223
Aaron J. Brown <aaron.joseph.brown@me.com>
2023-10-01 10:24:30 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Aaron Brown on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
